d71ac73f22b7d48d962543d8d2c6e9050281925f
Lukas Siemers <126496413+lsiemers@users.noreply.github.com>
2024-02-14 16:28:58 -0600

    Project.go

--- Project.go
+++ Project.go
@@ -1,61 +1,70 @@
 package main
 
-//
-//	Lukas Siemers - Project#1 - CSC386
-//
 import (
-	"fmt"     //Imports
-	"os"      //''
-	"os/exec" //''
+	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	var inputcommand string
-	fmt.Printf("Please enter a command:") // Printing Prompt
-	fmt.Scan(inputcommand)                // Taking User-Input
-	for inputcommand != "exit" {
-		if inputcommand == "ls" { //If user input is the same as the string
-			out, err := exec.Command("ls").Output()
-			output := string(out[:])
-			fmt.Println(output)
-			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-				fmt.Println("Error:", err) //Print Statement
-			}
+	for {
+		var inputcommand string
+		fmt.Printf("Please enter a command:") // Printing Prompt
+		if _, err := fmt.Scanln(&inputcommand); err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
 
-		} else if inputcommand == "wc" { //If user input is the same as the string
-			out, err := exec.Command("wc").Output()
-			output := string(out[:])
-			fmt.Println(output)
-			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-				fmt.Println("Error:", err) //Print Statement
-			}
-		} else if inputcommand == "mkdir" { //If user input is the same as the string
-			out, err := exec.Command("mkdir").Output()
-			output := string(out[:])
-			fmt.Println(output)
-			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-				fmt.Println("Error:", err) //Print Statement
-			}
-		} else if inputcommand == "cp" { //If user input is the same as the string
-			out, err := exec.Command("cp").Output()
-			output := string(out[:])
-			fmt.Println(output)
-			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-				fmt.Println("Error:", err) //Print Statement
-			}
-		} else if inputcommand == "mv" { //If user input is the same as the string
-			out, err := exec.Command("mv").Output()
-			output := string(out[:])
-			fmt.Println(output)
-			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-				fmt.Println("Error:", err) //Print Statement
-			}
-		} else if inputcommand == "whoami" { //If user input is the same as the string
-			whoami() //Calling whoami Function
-		} else if inputcommand == "exit" { //If user input is the same as the string
+		if inputcommand == "exit" { //If user input is the same as the string
 			fmt.Println("Exiting shell.") //Print Statement
 			return                        //Returning
-		} else {
+		}
+
+		switch inputcommand {
+		case "ls": //If user input is the same as the string
+			out, err := exec.Command("ls").Output()
+			if err != nil { //Error Handling
+				fmt.Println("Error:", err) //Print Statement
+			} else {
+				fmt.Println(string(out))
+			}
+
+		case "wc": //If user input is the same as the string
+			out, err := exec.Command("wc").Output()
+			if err != nil { //Error Handling
+				fmt.Println("Error:", err) //Print Statement
+			} else {
+				fmt.Println(string(out))
+			}
+
+		case "mkdir": //If user input is the same as the string
+			out, err := exec.Command("mkdir").Output()
+			if err != nil { //Error Handling
+				fmt.Println("Error:", err) //Print Statement
+			} else {
+				fmt.Println(string(out))
+			}
+
+		case "cp": //If user input is the same as the string
+			out, err := exec.Command("cp").Output()
+			if err != nil { //Error Handling
+				fmt.Println("Error:", err) //Print Statement
+			} else {
+				fmt.Println(string(out))
+			}
+
+		case "mv": //If user input is the same as the string
+			out, err := exec.Command("mv").Output()
+			if err != nil { //Error Handling
+				fmt.Println("Error:", err) //Print Statement
+			} else {
+				fmt.Println(string(out))
+			}
+
+		case "whoami": //If user input is the same as the string
+			whoami() //Calling whoami Function
+
+		default:
 			fmt.Println("Command not supported.")
 		}
 	}
@@ -63,6 +72,6 @@ func main() {
 
 func whoami() {
 	// Get user ID Function
-	uid := os.Getuid() //I could not comeup on my own with thism so I found this line in the internet
+	uid := os.Getuid()
 	fmt.Printf("User: %s, UserID: %d\n", os.Getenv("USER"), uid)
 }
