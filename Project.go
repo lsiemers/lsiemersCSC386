b96d86f80371e3f67e61024ee2ff941a62be957c
Lukas Siemers <126496413+lsiemers@users.noreply.github.com>
2024-02-14 16:14:39 -0600

    Project.go

--- Project.go
+++ Project.go
@@ -6,51 +6,58 @@ package main
 import (
 	"fmt"     //Imports
 	"os"      //''
-	"os/exec" //''	
+	"os/exec" //''
 )
 
 func main() {
 	var inputcommand string
 	fmt.Printf("Please enter a command:") // Printing Prompt
-	fmt.Scan(inputcommand)               // Taking User-Input
+	fmt.Scan(inputcommand)                // Taking User-Input
+	for inputcommand != "exit" {
+		if inputcommand == "ls" { //If user input is the same as the string
+			out, err := exec.Command("ls").Output()
+			output := string(out[:])
+			fmt.Println(output)
+			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
+				fmt.Println("Error:", err) //Print Statement
+			}
 
-	if inputcommand == "ls" { //If user input is the same as the string
-		cmd := exec.Command("ls") //Assigning command to cmd
-		err := cmd.Run()          //Executing the cmd variable
-		if err != nil {           //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-			fmt.Println("Error:", err) //Print Statement
+		} else if inputcommand == "wc" { //If user input is the same as the string
+			out, err := exec.Command("wc").Output()
+			output := string(out[:])
+			fmt.Println(output)
+			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
+				fmt.Println("Error:", err) //Print Statement
+			}
+		} else if inputcommand == "mkdir" { //If user input is the same as the string
+			out, err := exec.Command("mkdir").Output()
+			output := string(out[:])
+			fmt.Println(output)
+			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
+				fmt.Println("Error:", err) //Print Statement
+			}
+		} else if inputcommand == "cp" { //If user input is the same as the string
+			out, err := exec.Command("cp").Output()
+			output := string(out[:])
+			fmt.Println(output)
+			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
+				fmt.Println("Error:", err) //Print Statement
+			}
+		} else if inputcommand == "mv" { //If user input is the same as the string
+			out, err := exec.Command("mv").Output()
+			output := string(out[:])
+			fmt.Println(output)
+			if err != nil { //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
+				fmt.Println("Error:", err) //Print Statement
+			}
+		} else if inputcommand == "whoami" { //If user input is the same as the string
+			whoami() //Calling whoami Function
+		} else if inputcommand == "exit" { //If user input is the same as the string
+			fmt.Println("Exiting shell.") //Print Statement
+			return                        //Returning
+		} else {
+			fmt.Println("Command not supported.")
 		}
-	} else if inputcommand == "wc" { //If user input is the same as the string
-		cmd := exec.Command("wc") //Assigning command to cmd
-		err := cmd.Run()          //Executing the cmd variable
-		if err != nil {           //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-			fmt.Println("Error:", err) //Print Statement
-		}
-	} else if inputcommand == "mkdir" { //If user input is the same as the string
-		cmd := exec.Command("mkdir") //Assigning command to cmd
-		err := cmd.Run()             //Executing the cmd variable
-		if err != nil {              //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-			fmt.Println("Error:", err) //Print Statement
-		}
-	} else if inputcommand == "cp" { //If user input is the same as the string
-		cmd := exec.Command("cp") //Assigning command to cmd
-		err := cmd.Run()          //Executing the cmd variable
-		if err != nil {           //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-			fmt.Println("Error:", err) //Print Statement
-		}
-	} else if inputcommand == "mv" { //If user input is the same as the string
-		cmd := exec.Command("mv") //Assigning command to cmd
-		err := cmd.Run()          //Executing the cmd variable
-		if err != nil {           //Error Handeling (This took me a while to figure out, otherwise the .Run command was not gonna work)
-			fmt.Println("Error:", err) //Print Statement
-		}
-	} else if inputcommand == "whoami" { //If user input is the same as the string
-		whoami() //Calling whoami Function
-	} else if inputcommand == "exit" { //If user input is the same as the string
-		fmt.Println("Exiting shell.") //Print Statement
-		return                        //Returning
-	} else {
-		fmt.Println("Command not supported.")
 	}
 }
 
